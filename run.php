43e1529a2c42241b9dda710beb35a9f76bd1741b
Raymond Chong <mathsgod@yahoo.com>
2025-01-10 12:06:20 +0800

    🎨 改進：重命名工作流程文件並更新其內容 🔥 刪除：移除不再使用的Node.js CI工作流程 ✨ 新增：新增多語言日曆功能，支持英文、簡體中文和繁體中文 🔧 配置：更新日曆數據的下載和保存邏輯，確保數據的正確性和完整性

--- run.php
+++ run.php
@@ -1,7 +1,56 @@
 <?php
+class Calendar
+{
 
-//download the file from the server
-//https://www.1823.gov.hk/common/ical/en.json
-$content = file_get_contents('https://www.1823.gov.hk/common/ical/en.json');
+    public $data;
+    public $language;
+    public function __construct($language)
+    {
+        $this->language = $language;
+        $this->data = json_decode(file_get_contents(__DIR__ . "/data/$language.json"), true);
+    }
 
-file_put_contents(__DIR__ . "/data/" . date('YmdHis') . ".json", $content);
+
+    public function update($id, $date, $name)
+    {
+        foreach ($this->data as &$event) {
+            if ($event["id"] == $id) {
+                $event["name"] = $name;
+                $event["date"] = $date;
+                return;
+            }
+        }
+        $this->data[] = [
+            "id" => $id,
+            "name" => $name,
+            "date" => $date,
+        ];
+    }
+
+    public function save()
+    {
+        file_put_contents(__DIR__ . "/data/$this->language.json", json_encode($this->data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+    }
+}
+
+foreach (["en", "sc", "tc"] as $language) {
+    $calendar = new Calendar($language);
+
+    $data = file_get_contents("https://www.1823.gov.hk/common/ical/$language.json");
+    //clean the file bom and utf8
+    $data = preg_replace('/\x{FEFF}/u', '', $data);
+    $new_data = json_decode($data, true, 512, JSON_THROW_ON_ERROR);
+
+    $events = $new_data["vcalendar"][0]["vevent"];
+
+    foreach ($events as $event) {
+        $date = substr($event["dtstart"][0], 0, 4) . "-" . substr($event["dtstart"][0], 4, 2) . "-" . substr($event["dtstart"][0], 6, 2);
+        $name = $event["summary"];
+        $id = $event["uid"];
+
+        $calendar->update($id, $date, $name);
+    }
+
+    //save the data
+    $calendar->save();
+}
