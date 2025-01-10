a507af06e36c11fd538f8d8f9c1a1a1aa3fa7853
Raymond Chong <mathsgod@yahoo.com>
2025-01-10 11:33:15 +0800

    ✨ 新增(run.php)：從伺服器下載JSON檔案並儲存 💡 註解(run.php)：新增下載檔案的說明註解以提高可讀性
    這次的變更是為了實現從指定URL下載JSON檔案並將其儲存到當前目錄，這樣可以方便後續的數據處理和使用。

--- run.php
+++ run.php
@@ -1,2 +1,7 @@
 <?php
-echo "hello";
+
+//download the file from the server
+//https://www.1823.gov.hk/common/ical/en.json
+$content = file_get_contents('https://www.1823.gov.hk/common/ical/en.json');
+
+file_put_contents(__DIR__ . "/" . date('Y-m-d') . ".json", $content);
